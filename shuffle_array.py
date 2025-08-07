182d9c530b3c29b2c0a3acd833510f055a053d94
wisetoken12680 <srivastavaaarush234@gmail.com>
2025-08-07 15:21:16 +0530

    shuffle_array.py

---
+++ shuffle_array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        result=[]
+        for num in range(n):
+            result.append(nums[num])
+            result.append(nums[num+n])
+        return result
