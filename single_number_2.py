281224f976b4bcbe230f2bdbeb45a32dfd8d656f
wisetoken12680 <srivastavaaarush234@gmail.com>
2025-08-10 15:10:23 +0530

    single_number_2.py

---
+++ single_number_2.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        ones = 0
+        twos = 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
