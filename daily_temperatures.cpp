e8332255765b0ed35d4a50dd8a2a1870ed6d4e04
wisetoken12680 <srivastavaaarush234@gmail.com>
2026-03-24 23:20:06 +0530

    doneee

---
+++ daily_temperatures.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prev = st.top();
+                st.pop();
+                answer[prev] = i - prev;
+            }
+            st.push(i);
+        }
+        return answer;
+    }
+};
